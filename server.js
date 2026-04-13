26b853c586d4bfea41ca78d99a3bda87704fbe26
2089937081-dot <2089937081@qq.com>
2026-04-13 07:20:24 +0000

    Lab 1

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (request, response) => {
+ response.send('Welcome to MILA University!');
+});
+
+app.listen(PORT, () => {
+ console.log(`Server is successfully running on port ${PORT}`);
+});
