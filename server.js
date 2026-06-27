4492e0394161d50eafd00e4693c41f7bf919c8c8
2089937081-dot <2089937081@qq.com>
2026-06-27 09:03:08 +0000

    Lab 4

--- server.js
+++ server.js
@@ -23,6 +23,38 @@ return console.error(err.message);
 res.redirect('/'); 
 }); 
 }); 
+app.post('/delete/:id', (req, res) => { 
+const studentId = req.params.id; 
+db.run('DELETE FROM students WHERE id = ?', [studentId], (err) => { 
+if (err) { 
+return console.error(err.message); 
+} 
+res.redirect('/'); 
+}); 
+}); 
+app.get('/edit/:id', (req, res) => { 
+const studentId = req.params.id; 
+db.get('SELECT * FROM students WHERE id = ?', [studentId], (err, row) => { 
+if (err) { 
+return console.error(err.message); 
+} 
+res.render('edit', { student: row }); 
+}); 
+}); 
+app.post('/update/:id', (req, res) => { 
+    const studentId = req.params.id; 
+    const updatedName = req.body.studentName; 
+    const updatedEmail = req.body.studentEmail; 
+     
+    db.run('UPDATE students SET name = ?, email = ? WHERE id = ?', 
+[updatedName, updatedEmail, studentId], (err) => { 
+        if (err) { 
+            return console.error(err.message); 
+        } 
+        res.redirect('/'); 
+    }); 
+}); 
+ 
 app.listen(PORT, () => { 
-console.log(`Server is successfully running on port ${PORT}`); 
+    console.log(`Server is successfully running on port ${PORT}`); 
 });
