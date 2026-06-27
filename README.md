96f580792e860e4518b29a6d46e2174d5a22cdc7
2089937081-dot <2089937081@qq.com>
2026-06-27 08:11:10 +0000

    Lab 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Name: Li Bodonng
 ID: 1002500189
 
-<h1>Lab 1</h1>
+<h1>Lab 2</h1>
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
-
-app.get('/', (request, response) => {
- response.send('Welcome to MILA University!');
-});
-
-app.listen(PORT, () => {
- console.log(`Server is successfully running on port ${PORT}`);
-});
+const express = require('express'); 
+const app = express(); 
+const PORT = 3000; 
+app.set('view engine', 'ejs'); 
+app.use(express.urlencoded({ extended: true })); 
+app.get('/', (req, res) => { 
+res.render('index'); 
+}); 
+app.post('/submit', (req, res) => { 
+const submittedName = req.body.studentName; 
+const submittedEmail = req.body.studentEmail; 
+res.render('success', {  
+name: submittedName,  
+email: submittedEmail  
+}); 
+}); 
+app.listen(PORT, () => { 
+console.log(`Server is successfully running on port ${PORT}`); 
+}); 
